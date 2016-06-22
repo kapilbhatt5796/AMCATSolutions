c9c5c7ed51e84c4f405061f37328631347054ff1
kapilbhatt5796 <kapilbhatt5796@users.noreply.github.com>
2016-06-22 10:42:02 +0530

    Update sol1.cpp

--- sol1.cpp
+++ sol1.cpp
@@ -1,35 +1,37 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-#define X first
-#define Y second
-#define INPUT freopen("clash.inp","r",stdin)
-#define OUTPUT freopen("clash.out","w",stdout)
-#define FOR(i,l,r) for(auto i=(l);i<=(r);i++)
-#define REP(i,l,r) for(auto i=(l);i<(r);i++)
-#define FORD(i,l,r) for(auto i=(l);i>=(r);i--)
-#define REPD(i,l,r) for(auto i=(l);i>(r);i--)
-#define ENDL printf("\n")
-#define debug 1
-
-typedef long long ll;
-typedef pair<int,int> ii;
-
-const int inf=1e9;
-const int MOD=1e9+7;
-const int N=0;
-
-int main(){
-    scanf("%d",&test);
+typedef unsigned long long LL;
+// return (a^p)%mod
+LL power(LL a, int p, int mod)
+{
+    LL ret = 1;
+    while(p)
+    {
+        if(p&1)ret = (ret*a)%mod;
+        a=(a*a)%mod;
+        p/=2;
+    }
+    return ret;
+}
+int main()
+{
+    int t; cin>>t;
+    while(t--)
+    {
+        LL a,d; int p; 
+        cin>>a>>d>>p;
+        a%=p; d%=p;
         int n;
-        scanf("%d",&n);
-        int ans=0;
-        FOR(i,1,n) {
-            ll x,y,val;
-            scanf("%lld%lld%lld",&x,&y,&val);
-            if (val%2!=(x+y-2)%2) ans++;
+        if(a==0)
+            n=0;
+        else if(d==0)
+            n=-1;
+        else
+        {
+            LL x = power(d,p-2,p);
+            n = (x*(p-a))%p; 
         }
-        if (ans==0||ans==n) printf("Yes\n");
-        else printf("No\n");
+        cout<<n<<endl;
+    }
+    return 0;
 }
